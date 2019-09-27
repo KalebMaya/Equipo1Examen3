fe5e56014fb9fde3624f0868ee3407932c6f472e
volkodlak300 <volkodlak300@gmail.com>
2019-09-27 11:52:38 -0500

    eureka server
    se creo eureka server y se agregaron las configuraciones

---
+++ equipo1examen3-eureka-server/src/main/java/com/everis/equipo1examen3eurekaserver/Equipo1examen3EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.everis.equipo1examen3eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class Equipo1examen3EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo1examen3EurekaServerApplication.class, args);
+	}
+
+}
